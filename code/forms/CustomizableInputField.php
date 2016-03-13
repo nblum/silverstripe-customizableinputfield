c0a1ce5fc43973a8568773dbb23d8e850b0add20
Nico <mail@blum-nico.de>
2016-03-13 21:29:13 +0100

    minor updates

--- code/forms/CustomizableInputField.php
+++ code/forms/CustomizableInputField.php
@@ -11,6 +11,58 @@ class CustomizableInputField extends Text
         return self::Strval($this->value);
     }
 
+    /**
+     * checkes whether at least one part is not empty
+     * @return bool
+     */
+    public function NotEmpty()
+    {
+        return !$this::isEmpty($this->value);
+    }
+
+    public static function isEmpty($field)
+    {
+        $parts = json_decode($field);
+
+        if(empty($parts)) {
+            return true;
+        }
+
+        foreach ($parts as $part) {
+            if(!empty(trim($part))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    public function Val() {
+        $parts = json_decode($this->value);
+        $first = array_shift($parts);
+        return $first->val;
+    }
+
+    /**
+     * @return string
+     */
+    public function Float() {
+        return floatval(preg_replace('/,/', '.',self::Strval($this->value)));
+    }
+
+    /**
+     * formats the float value as currency
+     * use setlocale(LC_MONETARY, 'de_DE.utf8'); in your config file for correct format
+     * @return string
+     */
+    public function Money() {
+        return money_format('%+n', $this->Float());
+    }
+
+
     public static function Strval($value = '')
     {
         $strval = '';
